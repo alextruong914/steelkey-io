f7b0e7033e0af5715143437aee331ee982c8344b
quyanh1189 <quyanh159@gmail.com>
2022-09-07 20:04:38 +0700

    Checkout

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,56 @@
 import Head from 'next/head'
-
+import Image from 'next/image'
+import { checkout} from '../CheckoutForm'
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Learn NextJS</title>
+        <title>Steel Key Webstore</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Buy my steel keys
         </h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
 
+        {/* <div id="checkout">
+          <button>Pay now!</button>
+        </div> */}
+
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+            <Image src="/images/product1.webp" alt="NanaX" width={400} height={400} />
+            <p>NANO X</p>
+            <button onClick={(() => {
+              checkout({
+                lineItems: [
+                  {
+                    quantity: 1,
+                    price: "price_1LevnDKbatRxHBBGMY8PHxvB"
+                  }
+                ]
+              })
+            })}>BUY!</button>
+          </div>
+          <div>
+            <Image src="/images/product2.webp" alt="NanaSPlus" width={400} height={400} />
+            <p>NANO S PLUS</p>
+            <button onClick={(() => {
+              checkout({
+                lineItems: [
+                  {
+                    quantity: 1,
+                    price: "price_1Levp1KbatRxHBBGb7Ckralh"
+                  }
+                ]
+              })
+            })}>BUY!</button>
+          </div>
         </div>
       </main>
 
