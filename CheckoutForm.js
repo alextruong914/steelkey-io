146925efa9893669f68b1ddb39a658e5f1a831ca
quyanh1189 <quyanh159@gmail.com>
2022-09-07 20:15:12 +0700

    FixBug

--- CheckoutForm.js
+++ CheckoutForm.js
@@ -5,7 +5,7 @@ export async function checkout({lineItems}) {
 
     const getStripe = () => {
         if (!stripePromise) {
-            stripePromise = loadStripe(process.env.NEXT_PUBLIC_API_KEY);
+            stripePromise = loadStripe(`${process.env.NEXT_PUBLIC_API_KEY}`);
         }
         return stripePromise;
     }
